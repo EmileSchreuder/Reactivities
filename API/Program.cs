17525dc3c56474aa0b609b3ff3f341924a3e3eb7
iacdemile <schreuderemile3@gmail.com>
2023-08-30 22:14:19 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,13 @@ builder.Services.AddDbContext<DataContext>(optionsAction =>
 {
     optionsAction.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyOrigin().WithOrigins("http://localhost:3000");
+    });
+});
 
 var app = builder.Build();
 
@@ -23,6 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
